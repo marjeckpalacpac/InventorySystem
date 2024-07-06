2f5fb1756e0183814a3b25653ac326a5d9cc27e3
Marjeck Palacpac <marjeck@advabilities.com>
2024-07-06 19:37:14 +0800

    Add Services folder. Create ProductCategoryService

---
+++ Inventory.DataAccess/Services/ProductCategory/ProductCategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Inventory.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.DataAccess.Services
+{
+    public class ProductCategoryService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductCategoryService(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
